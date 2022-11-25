afd0b7e19d4b50e29a345e8b1b20428cfa4a68e7
Firsova Liubov <firsova.lyub@gmail.com>
2022-11-25 10:41:30 +0200

    added modal-loc-franc. html + scss

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,4 +1,4 @@
-const refs = {
+/*const refs = {
 	openModalBtn: document.querySelector("[data-open-modal]"),
 	closeModalBtn: document.querySelector("[data-close-modal]"),
 	backdrop: document.querySelector("[data-backdrop]"),
@@ -11,3 +11,4 @@ function toggleModal() {
 	document.body.classList.toggle("modal-open");
 	refs.backdrop.classList.toggle("is-hidden");
 }
+*/
