44b5a4586ad12d782394cc8f79e8ce42155e306d
Firsova Liubov <firsova.lyub@gmail.com>
2022-11-25 16:46:06 +0200

    added styles to modal-franc

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,23 +1,23 @@
-/*const refs = {
-	openModalBtn: document.querySelector("[data-open-modal]"),
-	closeModalBtn: document.querySelector("[data-close-modal]"),
-	backdrop: document.querySelector("[data-backdrop]"),
-};
-
-refs.openModalBtn.addEventListener("click", toggleModal);
-refs.closeModalBtn.addEventListener("click", toggleModal);
-
-function toggleModal() {
-	document.body.classList.toggle("modal-open");
-	refs.backdrop.classList.toggle("is-hidden");
-}
-*/
-
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('visually-hidden');
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-franc-open]'),
+    closeModalBtn: document.querySelector('[data-modal-franc-close]'),
+    modal: document.querySelector('[data-modal-franc]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
