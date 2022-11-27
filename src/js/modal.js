58c11d512bcad4d2cd474d5ac92962631e407f77
Dasha Yarkovska <yarkovska11@gmail.com>
2022-11-27 12:11:49 +0200

    transfer script into franc-modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -12,20 +12,3 @@
     refs.modal.classList.toggle('visually-hidden');
   }
 })();
-
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-franc-open]'),
-    closeModalBtn: document.querySelector('[data-modal-franc-close]'),
-    modal: document.querySelector('[data-modal-franc]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('visually-hidden');
-  }
-
-})();
-
