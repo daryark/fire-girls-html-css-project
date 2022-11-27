3960f2aa8212607760553d458194506367cc1acd
Firsova Liubov <firsova.lyub@gmail.com>
2022-11-27 22:50:24 +0200

    created html and scss for buy-now

---
+++ src/js/modal-buy-now.js
@@ -0,0 +1,13 @@
+const refs = {
+	openModalBtn: document.querySelector("[data-open-modal-buy-now]"),
+	closeModalBtn: document.querySelector("[data-close-modal-buy-now]"),
+	backdrop: document.querySelector("[data-backdrop-buy-now]"),
+};
+
+refs.openModalBtn.addEventListener("click", toggleModal);
+refs.closeModalBtn.addEventListener("click", toggleModal);
+
+function toggleModal() {
+	document.body.classList.toggle("data-open-modal-buy-now");
+	refs.backdrop.classList.toggle("is-hidden");
+}
