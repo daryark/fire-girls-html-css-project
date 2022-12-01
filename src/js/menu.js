768d373f310d9466197fcb38807e5b2f06e22440
Dasha Yarkovska <yarkovska11@gmail.com>
2022-12-01 22:45:08 +0200

    button in mob menu works

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,13 +1,13 @@
 const refs = {
-	openModalBtn: document.querySelector("[data-open-mob-menu]"),
-	closeModalBtn: document.querySelector("[data-close-mob-menu]"),
-	backdrop: document.querySelector("[data-mob-menu]"),
+  openModalBtn: document.querySelector('[data-open-mob-menu]'),
+  closeModalBtn: document.querySelector('[data-close-mob-menu]'),
+  backdrop: document.querySelector('[data-mob-menu]'),
 };
 
-refs.openModalBtn.addEventListener("click", toggleModal);
-refs.closeModalBtn.addEventListener("click", toggleModal);
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
 
 function toggleModal() {
-	document.body.classList.toggle("data-open-mob-menu");
-	refs.backdrop.classList.toggle("is-hidden");
+  document.body.classList.toggle('data-open-mob-menu');
+  refs.backdrop.classList.toggle('is-hidden');
 }
--- src/js/modal-buy-now.js
+++ src/js/modal-buy-now.js
@@ -1,13 +1,15 @@
 const refs = {
-	openModalBtn: document.querySelector("[data-open-modal-buy-now]"),
-	closeModalBtn: document.querySelector("[data-close-modal-buy-now]"),
-	backdrop: document.querySelector("[data-backdrop-buy-now]"),
+  openModalBtn: document.querySelector('[data-open-modal-buy-now]'),
+  openModalBtnMob: document.querySelector('[data-open-modal-buy-now-mob]'),
+  closeModalBtn: document.querySelector('[data-close-modal-buy-now]'),
+  backdrop: document.querySelector('[data-backdrop-buy-now]'),
 };
 
-refs.openModalBtn.addEventListener("click", toggleModal);
-refs.closeModalBtn.addEventListener("click", toggleModal);
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.openModalBtnMob.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
 
 function toggleModal() {
-	document.body.classList.toggle("data-open-modal-buy-now");
-	refs.backdrop.classList.toggle("is-hidden");
+  document.body.classList.toggle('data-open-modal-buy-now');
+  refs.backdrop.classList.toggle('is-hidden');
 }
