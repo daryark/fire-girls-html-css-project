c0d1a008d55a1cb5172118aa8e0fce586d49409b
Dasha Yarkovska <yarkovska11@gmail.com>
2023-01-29 15:04:08 +0200

    no-fix, just reload page to reload on gh-pages

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,30 +1,13 @@
 const refs = {
-	openModalBtn: document.querySelector("[data-open-modal]"),
-	closeModalBtn: document.querySelector("[data-close-modal]"),
-	backdrop: document.querySelector("[data-backdrop]"),
+  openModalBtn: document.querySelector('[data-open-modal]'),
+  closeModalBtn: document.querySelector('[data-close-modal]'),
+  backdrop: document.querySelector('[data-backdrop]'),
 };
 
-refs.openModalBtn.addEventListener("click", toggleModal);
-refs.closeModalBtn.addEventListener("click", toggleModal);
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
 
 function toggleModal() {
-	document.body.classList.toggle("data-open-modal");
-	refs.backdrop.classList.toggle("visually-hidden");
+  document.body.classList.toggle('data-open-modal');
+  refs.backdrop.classList.toggle('visually-hidden');
 }
-
-/*
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('visually-hidden');
-  }
-})();
-*/
